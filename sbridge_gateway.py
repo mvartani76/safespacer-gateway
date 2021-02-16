80ad432d90360b7412030f9433edb2e5be08a099
Mike Vartanian <mike.vartanian@gmail.com>
2021-02-16 14:51:27 -0500

    Integrate AWS IoT Python SDK code
    Integrated the sample python code for basicPubSubAsync.py
    
    https://github.com/aws/aws-iot-device-sdk-python/blob/master/samples/basicPubSub/basicPubSubAsync.py

--- sbridge_gateway.py
+++ sbridge_gateway.py
@@ -1,3 +1,7 @@
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import logging
+import argparse
+
 import serial
 import time
 import sys
@@ -12,6 +16,104 @@ BAUDRATE = 921600
 #ser.stopbits = serial.STOPBITS_ONE
 #ser.timeout = 2
 #ser.writeTimeout = 2
+
+# AWS IoT Code
+# General message notification callback
+def customOnMessage(message):
+    print("Received a new message: ")
+    print(message.payload)
+    print("from topic: ")
+    print(message.topic)
+    print("--------------\n\n")
+
+
+# Suback callback
+def customSubackCallback(mid, data):
+    print("Received SUBACK packet id: ")
+    print(mid)
+    print("Granted QoS: ")
+    print(data)
+    print("++++++++++++++\n\n")
+
+
+# Puback callback
+def customPubackCallback(mid):
+    print("Received PUBACK packet id: ")
+    print(mid)
+    print("++++++++++++++\n\n")
+
+
+# Read in command-line parameters
+parser = argparse.ArgumentParser()
+parser.add_argument("-e", "--endpoint", action="store", required=True, dest="host", help="Your AWS IoT custom endpoint")
+parser.add_argument("-r", "--rootCA", action="store", required=True, dest="rootCAPath", help="Root CA file path")
+parser.add_argument("-c", "--cert", action="store", dest="certificatePath", help="Certificate file path")
+parser.add_argument("-k", "--key", action="store", dest="privateKeyPath", help="Private key file path")
+parser.add_argument("-p", "--port", action="store", dest="port", type=int, help="Port number override")
+parser.add_argument("-w", "--websocket", action="store_true", dest="useWebsocket", default=False,
+                    help="Use MQTT over WebSocket")
+parser.add_argument("-id", "--clientId", action="store", dest="clientId", default="basicPubSub",
+                    help="Targeted client id")
+parser.add_argument("-t", "--topic", action="store", dest="topic", default="sdk/test/Python", help="Targeted topic")
+
+args = parser.parse_args()
+host = args.host
+rootCAPath = args.rootCAPath
+certificatePath = args.certificatePath
+privateKeyPath = args.privateKeyPath
+port = args.port
+useWebsocket = args.useWebsocket
+clientId = args.clientId
+topic = args.topic
+
+if args.useWebsocket and args.certificatePath and args.privateKeyPath:
+    parser.error("X.509 cert authentication and WebSocket are mutual exclusive. Please pick one.")
+    exit(2)
+
+if not args.useWebsocket and (not args.certificatePath or not args.privateKeyPath):
+    parser.error("Missing credentials for authentication.")
+    exit(2)
+
+# Port defaults
+if args.useWebsocket and not args.port:  # When no port override for WebSocket, default to 443
+    port = 443
+if not args.useWebsocket and not args.port:  # When no port override for non-WebSocket, default to 8883
+    port = 8883
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Init AWSIoTMQTTClient
+myAWSIoTMQTTClient = None
+if useWebsocket:
+    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId, useWebsocket=True)
+    myAWSIoTMQTTClient.configureEndpoint(host, port)
+    myAWSIoTMQTTClient.configureCredentials(rootCAPath)
+else:
+    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+    myAWSIoTMQTTClient.configureEndpoint(host, port)
+    myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+myAWSIoTMQTTClient.onMessage = customOnMessage
+
+# Connect and subscribe to AWS IoT
+myAWSIoTMQTTClient.connect()
+# Note that we are not putting a message callback here. We are using the general message notification callback.
+myAWSIoTMQTTClient.subscribeAsync(topic, 1, ackCallback=customSubackCallback)
+time.sleep(2)
+
+# End of AWS IoT Code
 
 ser = serial.Serial(SERIALPORT, baudrate = BAUDRATE, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
 time.sleep(0.1)
@@ -173,6 +275,7 @@ while True:
 							readOut = ser.readline().decode() + readOut
 							time.sleep(1)
 							print("Reading read: " + str(readOut))
+						myAWSIoTMQTTClient.publishAsync(topic, "New Message " + str(readOut), 1, ackCallback=customPubackCallback)
 				print(fileNameArray)
 			# need to disconnect from the device
 			print("Disconnect from tag")
