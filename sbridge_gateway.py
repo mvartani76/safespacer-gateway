02543ee40fb0a2486119f0afa10ec8abeffd8baf
mvartani76 <mike.vartanian@gmail.com>
2021-02-16 22:01:05 -0500

    Updated gitignore to include the AWS IoT credentials from git. Updated the parameters to read from .env file.

--- sbridge_gateway.py
+++ sbridge_gateway.py
@@ -5,7 +5,10 @@ import argparse
 import serial
 import time
 import sys
+import os
 import dateutil.parser as dp
+from dotenv import load_dotenv
+load_dotenv()
 
 SERIALPORT = "/dev/ttyUSB0"
 BAUDRATE = 921600
@@ -43,42 +46,28 @@ def customPubackCallback(mid):
     print("++++++++++++++\n\n")
 
 
-# Read in command-line parameters
-parser = argparse.ArgumentParser()
-parser.add_argument("-e", "--endpoint", action="store", required=True, dest="host", help="Your AWS IoT custom endpoint")
-parser.add_argument("-r", "--rootCA", action="store", required=True, dest="rootCAPath", help="Root CA file path")
-parser.add_argument("-c", "--cert", action="store", dest="certificatePath", help="Certificate file path")
-parser.add_argument("-k", "--key", action="store", dest="privateKeyPath", help="Private key file path")
-parser.add_argument("-p", "--port", action="store", dest="port", type=int, help="Port number override")
-parser.add_argument("-w", "--websocket", action="store_true", dest="useWebsocket", default=False,
-                    help="Use MQTT over WebSocket")
-parser.add_argument("-id", "--clientId", action="store", dest="clientId", default="basicPubSub",
-                    help="Targeted client id")
-parser.add_argument("-t", "--topic", action="store", dest="topic", default="sdk/test/Python", help="Targeted topic")
+# Read in command-line parameters from .env file
+host = os.getenv("HOST")
+rootCAPath = os.getenv("ROOTPATH")
+certificatePath = os.getenv("CERTIFICATEPATH")
+privateKeyPath = os.getenv("PRIVATEKEYPATH")
+useWebsocket = False
+clientId = os.getenv("CLIENTID")
+topic = os.getenv("TOPIC")
 
-args = parser.parse_args()
-host = args.host
-rootCAPath = args.rootCAPath
-certificatePath = args.certificatePath
-privateKeyPath = args.privateKeyPath
-port = args.port
-useWebsocket = args.useWebsocket
-clientId = args.clientId
-topic = args.topic
+#if args.useWebsocket and args.certificatePath and args.privateKeyPath:
+#    parser.error("X.509 cert authentication and WebSocket are mutual exclusive. Please pick one.")
+#    exit(2)
 
-if args.useWebsocket and args.certificatePath and args.privateKeyPath:
-    parser.error("X.509 cert authentication and WebSocket are mutual exclusive. Please pick one.")
-    exit(2)
-
-if not args.useWebsocket and (not args.certificatePath or not args.privateKeyPath):
-    parser.error("Missing credentials for authentication.")
-    exit(2)
+#if not args.useWebsocket and (not args.certificatePath or not args.privateKeyPath):
+#    parser.error("Missing credentials for authentication.")
+#    exit(2)
 
 # Port defaults
-if args.useWebsocket and not args.port:  # When no port override for WebSocket, default to 443
-    port = 443
-if not args.useWebsocket and not args.port:  # When no port override for non-WebSocket, default to 8883
-    port = 8883
+#if args.useWebsocket and not args.port:  # When no port override for WebSocket, default to 443
+#    port = 443
+#if not args.useWebsocket and not args.port:  # When no port override for non-WebSocket, default to 8883
+port = 8883
 
 # Configure logging
 logger = logging.getLogger("AWSIoTPythonSDK.core")
