d2a8fb5f8f090834cbf71cbb4f4b65f7e961d6b6
mvartani76 <mike.vartanian@gmail.com>
2021-02-26 20:57:55 -0500

    Updated install script to include ansible installation. Also added some comments and print statements to see progress.

--- ss_install.sh
+++ ss_install.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+
+# install python3 dependencies
+printf "Installing python3 dependencies...\n"
 pip3 install python-dateutil
 pip3 install python-dotenv
+
+
+# install ansible dependencies
+printf "Installing ansible dependencies...\n"
+sudo apt update
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
