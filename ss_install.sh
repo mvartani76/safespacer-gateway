d31d23393cb2de1a1dcc64742e3236f54f48ab15
mvartani76 <mike.vartanian@gmail.com>
2021-02-28 14:47:27 -0500

    Added the AWS IoT SDK install script.

--- ss_install.sh
+++ ss_install.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+# stop script on error
+set -e
+
+# Check to see if root CA file exists, download if not
+if [ ! -f ./root-CA.crt ]; then
+  printf "\nDownloading AWS IoT Root CA certificate from AWS...\n"
+  curl https://www.amazontrust.com/repository/AmazonRootCA1.pem > root-CA.crt
+fi
+
+# Check to see if AWS Device SDK for Python exists, download if not
+if [ ! -d ./aws-iot-device-sdk-python ]; then
+  printf "\nCloning the AWS SDK...\n"
+  git clone https://github.com/aws/aws-iot-device-sdk-python.git
+fi
+
+# Check to see if AWS Device SDK for Python is already installed, install if not
+if ! python -c "import AWSIoTPythonSDK" &> /dev/null; then
+  printf "\nInstalling AWS SDK...\n"
+  pushd aws-iot-device-sdk-python
+  python3 setup.py install
+  result=$?
+  popd
+  if [ $result -ne 0 ]; then
+    printf "\nERROR: Failed to install SDK.\n"
+    exit $result
+  fi
+fi
+
 # install python3 dependencies
 printf "Installing python3 dependencies...\n"
 pip3 install python-dateutil
